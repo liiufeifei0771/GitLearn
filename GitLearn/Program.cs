6da9dc8a3ed4a5f29c590adb484c49b70e681b2f
liiufeifei0771 <1601268093@qq.com>
2024-10-30 11:52:02 +0800

    添加一个Add方法

--- GitLearn/Program.cs
+++ GitLearn/Program.cs
@@ -11,6 +11,16 @@ namespace GitLearn
         static void Main(string[] args)
         {
             String ver = "1.0";
+            int a = 1;
+            int b = 2;
+            var result = Add(a, b);
+            Console.WriteLine("{0}+{1} result is {2}", a, b, result);
+            Console.Read();
+        }
+
+        private static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
